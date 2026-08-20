af171106cd9e003145e51ea93b8f20f9ac7bfee3
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-08-20 16:51:05 -0300

    feat: implementa LivrosController com operações CRUD completas

--- Controllers/LivrosController.cs
+++ Controllers/LivrosController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using GranBooks.API.Data;
+﻿using GranBooks.API.Data;
 using GranBooks.API.Models;
+using GranBooks.API1.Data;
+using GranBooks.API1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace GranBooks.API.Controllers
+namespace GranBooks.API1.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -21,6 +23,76 @@ namespace GranBooks.API.Controllers
         public async Task<ActionResult<IEnumerable<Livro>>> GetLivros()
         {
             return await _context.Livros.ToListAsync();
+        }
+
+        // GET: api/livros/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Livro>> GetLivro(int id)
+        {
+            var livro = await _context.Livros.FindAsync(id);
+
+            if (livro == null)
+            {
+                return NotFound();
+            }
+
+            return livro;
+        }
+
+        // POST: api/livros
+        [HttpPost]
+        public async Task<ActionResult<Livro>> PostLivro(Livro livro)
+        {
+            _context.Livros.Add(livro);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetLivro), new { id = livro.Id }, livro);
+        }
+
+        // PUT: api/livros/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLivro(int id, Livro livro)
+        {
+            if (id != livro.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(livro).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Livros.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/livros/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLivro(int id)
+        {
+            var livro = await _context.Livros.FindAsync(id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+
+            _context.Livros.Remove(livro);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
