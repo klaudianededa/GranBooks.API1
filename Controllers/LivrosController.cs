dc0d940e36a90d3ff2c7d4df609221ff4dafe9ff
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-08-20 16:45:25 -0300

    feat: configura banco de dados, migrations e cria LivrosController com GET

---
+++ Controllers/LivrosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GranBooks.API.Data;
+using GranBooks.API.Models;
+
+namespace GranBooks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public LivrosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/livros
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Livro>>> GetLivros()
+        {
+            return await _context.Livros.ToListAsync();
+        }
+    }
+}
