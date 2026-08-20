953fa7a1e576f85f4c09cb571dc8e7580582605f
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-08-20 16:29:20 -0300

    feat: cria entidade Livro com propriedades base

---
+++ Models/Livro.cs
@@ -0,0 +1,12 @@
+﻿namespace GranBooks.API1.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Autor { get; set; } = string.Empty;
+        public string Categoria { get; set; } = string.Empty;
+        public int Copias { get; set; }
+        public string Imagem { get; set; } = string.Empty;
+    }
+}
